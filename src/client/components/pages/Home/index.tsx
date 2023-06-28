1637726c391b404c8b590f99e3a3037d621544c0
Luis <luis.canali@theappmaster.com>
2023-06-28 16:49:38 -0300

    POR-18 create slider

--- src/client/components/pages/Home/index.tsx
+++ src/client/components/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import Slider from '../../molecules/homeSections/first';
 import './style.scss';
 
 const Home = () => (
@@ -5,7 +6,9 @@ const Home = () => (
     <div className="home__overview home__section">
       <div className="home__overview-container">
         <div className="home__overview-text">Your professional portfolio whenever you need it</div>
-        <div className="home__section home__features">FEATURES</div>
+        <div className="home__section home__features">
+          <Slider />
+        </div>
         <div className="home__section home__features">commin</div>
         <div className="home__section home__features">soon</div>
       </div>
